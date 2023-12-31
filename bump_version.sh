c0ea367eddf25603d1ca7abf459e377033a63fb8
Alex <mashkal2000@gmail.com>
2023-12-30 16:03:53 -0800

    Add script to bump the package version.
    The script does not do anything dangerous - all changes are local.
    It asks you to run the dangerous changes - pushing the tag and
    publishing the crate.
    
    This should ensure that the git tags and crate versions are always up to
    date.

---
+++ bump_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 1 ]
+then
+    echo 'Provide a new version number'
+    exit 1
+fi
+
+git tag "$1"
+sed -i "s/version = .*/version = \"$1\"/" Cargo.toml
+
+echo 'Now run:'
+echo ''
+echo "git push origin $1"
+echo 'cargo publish'
+echo ''
