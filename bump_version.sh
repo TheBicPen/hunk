ba9c17db3b4754abace7dcddda1a50e98f8eff6d
Alex <mashkal2000@gmail.com>
2024-05-22 21:01:46 -0700

    bump_version.sh: Commit before creating a tag
    This ensures that the tag points to the commit that updated the cargo
    files rather than trailing behind it by 1 commit.
    
    Note that updating the TOML file mysteriously updates the lock file as
    well, but with a delay. The "cargo update --workspace" call removes any
    race conditions related to this.

--- bump_version.sh
+++ bump_version.sh
@@ -8,8 +8,14 @@ then
     exit 1
 fi
 
-git tag "$1"
 sed -i "s/version = .*/version = \"$1\"/" Cargo.toml
+
+# Update the lock file to match the toml file
+cargo update --workspace
+
+git add Cargo.lock Cargo.toml
+git commit -m "Release $1"
+git tag "$1"
 
 echo 'Now run:'
 echo ''
